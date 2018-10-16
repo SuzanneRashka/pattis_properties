d8bc5f691410dc4a8784b79fbdae8316fb7b68b7
SuzanneRashka <suzanne.rashka@gmail.com>
2018-10-16 16:56:50 -0500

    client server seperated

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const routes = require("./routes");
-const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
+// const routes = require("./routes");
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(require("body-parser").text());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+    app.use(express.static("client/build"));
 }
 
-app.use(routes);
+// app.use(routes);
 
+app.use(express.static(path.join(__dirname + '/client/build/')))
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/do4u_db", { useNewUrlParser: true });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
-
- app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
- });
+app.listen(PORT, function () {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
