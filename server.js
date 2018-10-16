d15a81c3264100dbcc53fb659aedb0e0c5004bfa
SuzanneRashka <suzanne.rashka@gmail.com>
2018-10-16 16:17:09 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(require("body-parser").text());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use(routes);
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/do4u_db", { useNewUrlParser: true });
+
+
+ app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+ });
